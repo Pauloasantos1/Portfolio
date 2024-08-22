f96471954c0d3b8a21e36f81b470501a8a1ee52e
Paulo Santos <pauloa2018@gmail.com>
2024-08-22 11:46:35 -0400

    removed crater from blocking

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = ({ scrollContainer }) => {
         </div>
         <div className="flex-1 flex justify-start lg:justify-end mt-4 sm:mt-14 ml-8 xs:ml-[-4vh] sm:ml-[-17vh] md:ml-[-26vh] lg:mt-10 2xl:mt-0">
           <div className='font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left'>
-            My passion is <br/> bridging the gap between cutting edge technology and humans.
+            My passion is <br/> bridging the gap between cutting edge technology and society.
           </div>
         </div>
       </div>
@@ -22,10 +22,15 @@ const Hero = ({ scrollContainer }) => {
       <img className="parallax__planets" src="./parallax/2Planets.svg" alt="" />
       <img className="parallax__mountain1" src="./parallax/3Mountain.svg" alt="" />
       <img className="parallax__mountain2" src="./parallax/4Mountain.svg" alt="" />
-      <img className="parallax__crater" src="./parallax/5Crater.svg" alt="" />
+      {/* <img className="parallax__crater" src="./parallax/5Crater.svg" alt="" /> */}
       <img className="parallax__sun" src="./parallax/6Sun.svg" alt="" />
 
-      <SpacemanCanvas scrollContainer={scrollContainer} />
+      <div 
+        className="scroll-container z-10 relative"
+        style={{ zIndex: 10, position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+      >
+        <SpacemanCanvas scrollContainer={scrollContainer} />
+      </div>
     </section>
   );
 };
